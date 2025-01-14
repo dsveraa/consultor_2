fa2345e78b730d6fecf3e29a199d8693d616615b
dsveraa <dsvera.a@gmail.com>
2025-01-13 21:35:03 -0300

    first commit

---
+++ datos.js
@@ -0,0 +1,31 @@
+datos = [
+    {
+        examen: 'VIH',
+        keywords: ['sida', 'putas'],
+        establecimiento: "Santiago - Barnafi (1 semana)",
+        codigo: '0001',
+        codigoBK: 'BK0001',
+        nota: 'Nota sobre el examen'
+    },
+    {
+        examen: 'Orina completa',
+        keywords: ['meao', 'pichi'],
+        establecimiento: "Clinica Adventista",
+        codigo: '0002',
+        nota: 'Nota sobre el examen'
+    },
+    {
+        examen: 'GLUCOSA',
+        keywords: ['azucar', 'glicemia', 'sida'],
+        establecimiento: "Clinica Adventista",
+        codigo: '0003',
+        nota: 'Nota sobre el examen'
+    },
+    {
+        examen: 'Ácido Fólico',
+        keywords: ['Ac Folico'],
+        establecimiento: "Clinica Adventista",
+        codigo: '0003',
+        nota: 'Nota sobre el examen'
+    },
+]
